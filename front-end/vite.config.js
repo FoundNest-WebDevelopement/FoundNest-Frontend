c10e68d65614bb7f2341a151908861db4ef0f8f3
James Ian S. Antonio <jamesantonio0063@gmail.com>
2026-07-06 04:25:45 +0800

    debugging

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify('http://localhost:5000')
-  }
+  plugins: [react(), tailwindcss()]
 })
